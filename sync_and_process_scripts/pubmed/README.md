1ac9e7f5af19b997b7dde9880d8a4920fae6d923
Diego Munoz <diego.munoz.me@gmail.com>
2024-11-03 12:06:16 -0700

    downloads pubmed files, needs to include manifest

---
+++ sync_and_process_scripts/pubmed/README.md
@@ -0,0 +1,63 @@
+Pubmed is an FTP Server. 
+
+List of all zip files on pubmed FTP server
+https://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_file_list.txt
+
+
+FTP Where all the files are stored
+
+https://ftp.ncbi.nlm.nih.gov/pub/pmc/oa_package/
+
+``` python
+
+
+"""
+Output file structure for pubmed
+
+oa_package/
+    00/
+        00/
+        01/
+    01/
+        00/
+        01/
+            PMC1214.tar.gz
+
+Get first level directories
+For each first level directories
+    Get second level directories
+    For each second level directories
+        List files within second level directory
+        For each file in second level directory
+            Get metadata of file in FTP server
+            if (last updated data of file is greater than last ran script) or (File does not exist)
+                download file
+                update manifest with new updated timestamp
+
+=======================================================================================================
+                    
+first_level_dir = get_first_level_dir(ftp, main_dir) 
+for first_level_entry in first_level_dir:
+    updated_manifest = process_first_level_dir(first_level_entry, manifest, base_output_dir)      
+
+=======================================================================================================
+
+process_first_level_dir: input -> first_level_entry, manifest, base_output_dir
+if output_dir_for_first_level_entry does not exist
+    make dir for first_level_entry
+
+second_level_dir = get_second_level_dir(ftp, first_level_entry)
+for second_level_entry in second_level_dir:
+    updated_manifest = process_second_level_dir(second_level_entry, manifest, base_output_dir, first_level_entry)
+=======================================================================================================
+              
+process_second_level_dir: input -> second_level_entry, manifest, base_output_dir, first_level_entry
+if output_dir_for_second_level_entry does not exist
+    make dir for second_level_entry
+
+files_within_second_level = get_files_in_second_level_dir(ftp, second_level_entry)
+for filename in files_within_second_level:
+    process_file(first_level_entry, second_level_entry, manifest, filename)
+
+"""
+```
--- sync_and_process_scripts/pubmed/get_pubmed.py
+++ sync_and_process_scripts/pubmed/get_pubmed.py
@@ -1,16 +1,95 @@
 import ftplib
+from pathlib import Path
 
-def connect_to_pubmed():
-    ftp_server = 'ftp.ncbi.nlm.nih.gov'
+
+def connect_to_pubmed(ftp_server:object, directory:str) -> object:
+   
     ftp = ftplib.FTP(ftp_server)
+    # Login anonymous user and password
     ftp.login()
+    # cwd is change into /dir
+    ftp.cwd(directory)
+    return ftp
 
-    ftp.cwd('/pubmed/baseline/')
-    filenames = ftp.nlst()
+    
 
-    print(len(filenames))
+def process_first_level_directory(ftp, base_output:str)->list[str]:
+    # filenames = ftp.nlst()
+    files = []
+    # get filenames
+    ftp.retrlines('NLST',files.append)
+    for file in files:
+        output_path = Path(f'{base_output}/{file}')
+        output_path.mkdir(parents=True, exist_ok=True)
+    return files
 
+def process_second_level_directory(ftp:object, parent_dir:list[str])->list[str]:
+    """
+    for every parent folder, 
+    create the sub folder directory
+    """
+    for dir in parent_dir:
+        # enter into that dir in the ftp
+        ftp.cwd(dir)
+        folders = []
+        # get all folders that belong to the parent directory
+        ftp.retrlines('NLST',folders.append)
+        for folder in folders:
+            output_path = Path(f'{base_output}/{dir}/{folder}')
+            # create child directorys
+            output_path.mkdir(parents=True, exist_ok=True)
+            write_files_to_directory(ftp,output_path=output_path, sub_dir=folder)
+            
+        # back out, to create sub directories for other folders
+        ftp.cwd('..')
+        # return after first folder filled to make sure function works correctly
 
+def write_files_to_directory(ftp:object, output_path:Path, sub_dir:str ): 
+    """
+    download files into appropriate directory
+    """
+    manifest = []
+    ftp.cwd(sub_dir)
+    files = []
+    ftp.retrlines('NLST', files.append)
+    for file in files:
+        #output file destination 
+        output_file_path = output_path / file
+        if file not in manifest:
+            print(f'downloading {file}')
+            with open(output_file_path, 'wb') as f:
+                # download but do not extract zip files
+                ftp.retrbinary(f'RETR {file}', f.write)
+    #back out of sub_dir to download files for next dir
+    ftp.cwd('..')   
 
 if __name__ == "__main__":
-    connect_to_pubmed()
+    ftp_server = 'ftp.ncbi.nlm.nih.gov'
+    main_dir = r'/pub/pmc/oa_package' 
+
+    base_output = r''
+    # Make sure FTP server is live and we can still connect to it    
+    ftp = connect_to_pubmed(ftp_server=ftp_server, directory= main_dir)
+
+    # create output top structure
+    parent_folders = process_first_level_directory(ftp=ftp, base_output=base_output)
+
+    #create second level directory
+    second_level_dir = process_second_level_directory(ftp, parent_dir = parent_folders )
+
+    # write output .gzip files per directory
+
+
+
+
+
+
+
+
+
+    # populate output data
+
+    # disconnect from FTP
+    ftp.quit()
+
+
