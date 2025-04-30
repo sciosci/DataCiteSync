20c8d54ede715fd87895a06ce75cf80833c9b8b9
Diego Munoz <diego.munoz.me@gmail.com>
2025-04-30 12:14:23 -0600

    Updated create_manifest_template to contain slurm directives

--- sync_and_process_scripts/pubmed/create_manifest/create_manifest_template.sh
+++ sync_and_process_scripts/pubmed/create_manifest/create_manifest_template.sh
@@ -1,14 +1,20 @@
-#!/bin/bash
+#SBATCH --partition=<partition_name>
+#SBATCH --nodes=<nodes
+#SBATCH --ntasks=<tasks>
+#SBATCH --gres=<gpus>
+#SBATCH --time=<time>
+#SBATCH --output=<time>
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=<email>
 
-# Initialize Conda for Bash
-source ~/miniconda3/etc/profile.d/conda.sh
+module purge
+module load anaconda/2023.09
 
-# Activate the Conda environment
-conda activate intro-sos
+
+conda activate <path_to_conda_env> 
 
 # Execute the Python script
 python create_remote_manifest.py --input_dir  <input_directory>
 
 # Deactivate the Conda environment
 conda deactivate
-
